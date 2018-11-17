584bc776e529908b272aebca1f29f1a3e1f2ce89
Aby Abraham <abyabrkal@gmail.com>
2018-11-17 09:43:55 +0400

    Say Hello First

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+		g++ borasus.cpp -Wall -Wextra -std=c++11 -o borasus 
---
+++ borasus.cpp
@@ -0,0 +1,14 @@
+/******************************************
+ *  BORASUS 
+ *   - A Text Editor in C++
+ ******************************************/
+
+#include<iostream>
+
+
+int main() {
+
+	std::cout << "Hello World CPP" << std::endl;
+
+  	return 0;
+}
